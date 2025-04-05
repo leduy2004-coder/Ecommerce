47d4cba9e73a16facc58710a20644c7c7f4170c5
leduy2004-coder <leduy8762@gmail.com>
2025-04-05 14:39:18 +0700

    init recommendation and communication service

---
+++ communication-service/src/main/java/com/ecommerce/communication/CommunicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.communitcation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitcationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunitcationServiceApplication.class, args);
+	}
+
+}
---
+++ communication-service/src/test/java/com/ecommerce/communitcation_service/CommunitcationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.communitcation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitcationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
